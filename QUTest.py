6d88c9b5f9a98c59962ac0a363a8309ae1e0347f
James Wu <jameswu@cmu.edu>
2013-12-22 12:54:56 -0500

    Added Suites, sets of unit tests

--- QUTest.py
+++ QUTest.py
@@ -25,20 +25,24 @@ class Test(object):
 		"""
 		self.name = name
 		self.inputs = inputs
-		self.output = output
 		self.error = error
+		if error:
+			self.output = "Assertion Error"
+		else:
+			self.output = output
 		self.fn = fn
 
 	
 	def run(self):
 		"""
-		Returns a triplet, True means passed, False means failed. 
+		Returns a triplet, of type
+		(bool, int, Object)
+		True means passed, False means failed. 
 		Error codes:
 		0: Test Passed
 		1: Unexpected AssertionError
 		2: Expected AssertionError, code ran to completion
 		3: Incorrect Output
-		4: No Output Specified: Prints output
 		"""
 		
 		try:
@@ -49,28 +53,87 @@ class Test(object):
 
 			return (False, 1 ,e)
 
-		if self.output != None:
-			if self.error:
-				return (False, 2, None)
 
-			if testOutput == self.output:
-				return (True, 0, None)
+		if self.error:
+			return (False, 2, testOutput)
+
+		if testOutput == self.output:
+			return (True, 0, None)
+
+		else:
+			return (False, 3, testOutput)
+
+
+
+class Suite(object):
+	"""
+	A suite is a group of tests meant to be run together
+	"""
+	tests = []
+	def __init__(self, name = "", tests= []):
+		self.tests = tests
+		self.name = name
+
+	def addTest(self, test):
+		assert type(test) == Test
+		self.tests.append(test)
+		
+
+
+	def runTests(self):
+		print "Running unit tests on suite", self.name
+		print "--------------------------"
+		errorCount = 0
+		passed = 0
+
+		for test in self.tests:
+			print "Running", test.name
+			print "Input: ", test.inputs
+
+			print "Expect:", test.output
+
+			result = test.run()
+			if result[0]:
+				passed += 1
+				print "test successful"
+				print "--------------------------"
 
 			else:
-				return (False, 3, testOutput)
+				errorCount += 1
+				#otherwise there's an error
+				print "TEST FAILED"
+				print "Expected ", test.output
 
-		#Return the output
-		return (True, 4, testOutput)
+				#assertion error
+				if result[1] == 1:
+					print "An assertion error occurred unexpectedly:"
+					print result[2]
+
+				#expected an assertion error	
+				elif result[1] == 2:
+					print "Expected an assertion to fail, but ran to completion"
+					print "Output: ", result[2]
+
+				else:
+					assert result[1] == 3
+					print "Incorrect output:", result[2]
+
+				print "--------------------------"
+		print "Tests passed: ", passed
+		print "Tests failed: ", errorCount
+
+
+		return errorCount
 
 
 
 
 
-class QUTests(object):
-	tests = []
-	"""
-	Main testing suite. Use to add Tests and run them accordingly
-	"""
+
+
+
+
+
 
 	
 
--- testTest.py
+++ testTest.py
@@ -1,16 +1,26 @@
-from Test import *
+from QUTest import *
 
 def foo():
 	assert False
 	return 0
 
-test = Test(foo,[], output = 0, error = True)
-print test.run()
+test = Test(foo,[], output = 0, error = True, name = "foo")
+test2 = Test(test.run, [], (True, 0, None), name = "Test.run()")
 
 
+s = Suite("QUTest", [test])
+
+test3 = Test(s.addTest, [test2], None, error = False, name = "suite.addTest")
+
+test4 = Test(s.addTest, [5], None, error = True, name = "Improper input to suite.addTest")
 
 
+s.addTest(test3)
+s.addTest(test4)
 
-test2 = Test(test.run, [], (True, 0, None), name = "")
 
-print test2.run()
+test5 = Test(s.runTests, [], 0, error = False, name = "running tests")
+
+
+s.runTests()
+
